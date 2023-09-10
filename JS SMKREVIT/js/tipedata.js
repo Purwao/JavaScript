89915e048ab592dd118ec50568bf1838836738f6
Purwaa <140401408+Purwao@users.noreply.github.com>
2023-09-10 21:00:56 +0700

    Javascript SMKREVIT
    vid 7

---
+++ JS SMKREVIT/js/tipedata.js
@@ -0,0 +1,29 @@
+let kata= "balajar js";
+let angka= 100;
+ 
+console.log(angka);
+console.log(typeof kata);
+
+console.log(1/0);
+
+console.log("javascript"/ 2*3);
+
+let isi="belajar";
+
+console.log(kata + isi );
+
+console.log(`punyae purwa,anti copy ${kata} ${isi} ${angka}`);
+
+console.log(angka + kata + "" + isi);
+
+let kondisi= false;
+
+console.log(kondisi);
+
+let umur= null;
+
+console.log(umur);
+
+let empty;
+
+console.log(empty);
---
+++ JS SMKREVIT/js/var.js
@@ -0,0 +1,16 @@
+let angka   ;
+let kata    = "Belajar";
+const data= 100;
+
+angka=2;
+
+console.log(angka);
+console.log(kata);
+console.log(data);
+
+angka=8;
+
+console.log(angka);
+
+
+console.log(typeof kata);
